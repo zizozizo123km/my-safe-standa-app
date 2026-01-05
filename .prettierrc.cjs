05bc1ef853af6d918e6a7c985a726156a392d2f6
zizozizo123km <nnacar12@gmail.com>
2026-01-05 01:11:53 +0100

    Architect: .prettierrc.cjs

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  plugins: [
+    'prettier-plugin-tailwindcss'
+  ],
+};
